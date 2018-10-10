e4b4e34526671756d5a28e604ab2d886678ad81c
kdesignbrasil <44009475+kdesignbrasil@users.noreply.github.com>
2018-10-09 20:13:52 -0400

    Update MysqlConnect.php

--- MysqlConnect.php
+++ MysqlConnect.php
@@ -8,7 +8,7 @@ class MysqlConnect{
     private static $pdo;
     public  static function Conecta_Db() {
         try {
-            self::$pdo = new PDO("mysql:host=localhost;dbname=ilibert;charset=utf8", 'root', '');
+            self::$pdo = new PDO("mysql:host=localhost;dbname=DATANAME;charset=utf8", 'USER', 'SENHA');
             self::$pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
             self::$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             return self::$pdo;
